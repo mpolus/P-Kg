ffb42a778942e1eedeef20c3aa389bc803541008
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-07 13:32:06 -0400

    elmosworld

--- src/ptok.java
+++ src/ptok.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class ptok {
+public class ptok {
+    public static void main(String[] args) {
+        double pounds = 200.00;
+        double kilo = pounds * 0.45359237;
+        System.out.println(kilo);
+
+    }
 }
